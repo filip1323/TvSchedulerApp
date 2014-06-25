4fc8e26dbbdae2f4a0f07cbb76d428a4467da77e
toddler2584 <toddler2584@gmail.com>
2014-06-25 18:32:11 +0200

    Complete, unchecked

--- src/local_data/Settings.java
+++ src/local_data/Settings.java
@@ -5,10 +5,44 @@
  */
 package local_data;
 
+import java.util.HashMap;
+import user_exceptions.DebugError;
+
 /**
  *
  * @author Filip
  */
 public class Settings {
 
+    private HashMap<String, String> configCollection = null;
+
+    private static Settings instance = null;
+
+    public static Settings getInstance() {
+	if (instance == null) {
+	    instance = new Settings();
+	}
+	return instance;
+    }
+
+    private Settings() {
+	configCollection = new HashMap<>();
+	HashMap<String, String> results = Database.getInstance().getConfigProperties();
+	for (String key : results.keySet()) {
+	    configCollection.put(key, results.get(key));
+	}
+    }
+
+    public String getPropertyString(String key) {
+	if (!configCollection.containsKey(key)) {
+	    throw new DebugError(key);
+	}
+	return configCollection.get(key);
+    }
+
+    public void setProperty(String key, String value) {
+	Database.getInstance().setConfigProperty(key, value);
+	configCollection.remove(key);
+	configCollection.put(key, value);
+    }
 }
