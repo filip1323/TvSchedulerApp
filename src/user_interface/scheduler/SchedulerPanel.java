f35aacbb468760b39e6fb58679a4a48b91cd5dbc
toddler2584 <toddler2584@gmail.com>
2014-06-26 23:47:27 +0200

    unchecked

--- src/user_interface/scheduler/SchedulerPanel.java
+++ src/user_interface/scheduler/SchedulerPanel.java
@@ -3,39 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package user_interface;
+package user_interface.scheduler;
 
 import com.alee.extended.panel.GroupPanel;
+import com.alee.extended.panel.WebAccordion;
+import com.alee.extended.panel.WebAccordionStyle;
 import com.alee.laf.button.WebButton;
-import java.awt.Color;
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
+import com.alee.laf.panel.WebPanel;
+import user_interface.UserActionResponder;
 
 /**
  *
  * @author Filip
  */
-public class ContentPane extends JPanel {
+public class SchedulerPanel extends WebPanel {
 
     private GroupPanel contentGroupPanel;
     private WebButton exitButton;
+    private UserActionResponder userActionResponder;
+    private WebAccordion accordion;
+
+    public void assignUserActionResponder(UserActionResponder userActionResponder) {
+	this.userActionResponder = userActionResponder;
+    }
 
     public void initComponents() {
-	//-----------------------------BODY-----------------------------//
-	//creating exit button
-	exitButton = new WebButton("Exit");
+	accordion = new WebAccordion(WebAccordionStyle.accordionStyle);
+	accordion.setMultiplySelectionAllowed(false);
 
-	//-----------------------------CONTENT-----------------------------//
-	//creating contentGroupPanel panel
-	contentGroupPanel = new GroupPanel(0, false,
-		exitButton
-	);
+	contentGroupPanel = new GroupPanel(4, accordion);
+	accordion.setOrientation(0);
+	add(contentGroupPanel);
+	//setBorder(BorderFactory.createLineBorder(Color.black, 1, true));
+    }
 
-	//setting outer maring
-	contentGroupPanel.setMargin(5);
-
-	//-----------------------------FINAL-----------------------------//
-	add(contentGroupPanel, this);
-	setBorder(BorderFactory.createLineBorder(Color.black, 1, true));
+    public void addShowPanel(String title, GroupPanel content) {
+	accordion.addPane(title, content);
+	//content.setMargin(5);
     }
 }
