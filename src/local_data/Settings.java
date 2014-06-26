76d49a44d2882ed88aea7b8389e6a034663df1d3
toddler2584 <toddler2584@gmail.com>
2014-06-26 23:45:11 +0200

    development

--- src/local_data/Settings.java
+++ src/local_data/Settings.java
@@ -5,48 +5,92 @@
  */
 package local_data;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashMap;
-import user_exceptions.DebugError;
 
 /**
  *
  * @author Filip
  */
-public class Settings {
+public class Settings implements Serializable {
 
     private HashMap<String, String> configCollection = null;
 
-    public static final String BASIC_TVCOM_URL = "http://www.tv.com/";
-    public static final String SEARCH_TVCOM_URL = "http://www.tv.com/search?q=";
-    public static final String SHOW_TVCOM_URL = "http://www.tv.com/shows/";
+    public final String BASIC_TVCOM_URL = "http://www.tv.com/";
+    public final String SEARCH_TVCOM_URL = "http://www.tv.com/search?q=";
+    public final String SHOW_TVCOM_URL = "http://www.tv.com/shows/";
 
-    private static Settings instance = null;
+    public boolean OPTION_AUTOSTART = false;
+    public boolean OPTION_AUTOREFRESH = false;
+    public boolean OPTION_DEBUG = false;
+
+    public boolean OPTION_CONNECT_PIRATEBAY = false;
+    public boolean OPTION_CONNECT_EKINO = false;
+
+    //TODO option for displaying next ep for me
+    //TODO option for displaying next ep absolue
+    public boolean NOTIFICATION_NEXT_EP_COUNTER = false;
+    public boolean NOTIFICATION_NEXT_EP_ANNOUNCEMENT = false;
+    public boolean NOTIFICATION_NEXT_EP_RELEASED_TODAY = false;
+    public boolean NOTIFICATION_NEXT_SEAS_RELEASED_TODAY = false;
+    public boolean NOTIFICATION_UPDATE = false;
+
+    private transient static Settings instance = null;
 
     public static Settings getInstance() {
 	if (instance == null) {
 	    instance = new Settings();
+	    Settings loaded = instance.loadMe();
+	    if (loaded != null) {
+		instance = loaded;
+	    }
 	}
 	return instance;
     }
 
     private Settings() {
-	configCollection = new HashMap<>();
-	HashMap<String, String> results = Database.getInstance().getConfigProperties();
-	for (String key : results.keySet()) {
-	    configCollection.put(key, results.get(key));
+    }
+
+    public void saveMe() {
+	String path = "settings";
+	try {
+	    File file = new File(path);
+	    if (!file.exists()) {
+		file.createNewFile();
+	    }
+	    FileOutputStream fileOut
+		    = new FileOutputStream(path);
+
+	    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	    out.writeObject(this);
+	    out.close();
+	    fileOut.close();
+	} catch (IOException i) {
+	    i.printStackTrace();
 	}
     }
 
-    public String getPropertyString(String key) {
-	if (!configCollection.containsKey(key)) {
-	    throw new DebugError(key);
+    private Settings loadMe() {
+	String path = "settings";
+	Settings settings = null;
+	try {
+	    FileInputStream fileIn = new FileInputStream(path);
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    settings = (Settings) in.readObject();
+	    in.close();
+	    fileIn.close();
+	} catch (IOException i) {
+	    return null;
+	} catch (ClassNotFoundException c) {
+	    c.printStackTrace();
 	}
-	return configCollection.get(key);
-    }
 
-    public void setProperty(String key, String value) {
-	Database.getInstance().setConfigProperty(key, value);
-	configCollection.remove(key);
-	configCollection.put(key, value);
+	return settings;
     }
 }
---
+++ src/show_components/ShowLocalDataHUB.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package show_components;
+
+import external_websites.tvcom.TvcomSearchList;
+import external_websites.tvcom.TvcomSearchList.Result;
+import external_websites.tvcom.TvcomSeasonGuide;
+import external_websites.tvcom.TvcomShowHomepage;
+import external_websites.tvcom.TvcomUtils;
+import local_data.Settings;
+import show_components.episode.Episode;
+import show_components.episode.EpisodeBuilder;
+import show_components.episode.EpisodeTvcomInfo;
+import show_components.season.Season;
+import show_components.season.SeasonBuilder;
+import show_components.season.SeasonTvcomInfo;
+import show_components.show.Show;
+import show_components.show.ShowBuilder;
+import show_components.show.ShowTvcomInfo;
+import user_exceptions.DataNotAssignedException;
+import user_exceptions.DebugError;
+import user_exceptions.WrongUrlException;
+
+/**
+ *
+ * @author Filip
+ */
+public class ShowOnlineEngineer {
+
+    private String title;
+    private Show show;
+
+    public void assignTitle(String title) {
+	this.title = title;
+    }
+
+    public void constructShow() throws DataNotAssignedException, WrongUrlException {
+	if (title == null) {
+	    throw new DataNotAssignedException("title");
+	}
+	//creating search list link
+	String searchListUrl = Settings.SEARCH_TVCOM_URL + title;
+	TvcomSearchList searchList = new TvcomSearchList(searchListUrl);
+
+	//getting first result
+	Result firstResult = searchList.getFirstResult();
+	//make sure its right
+	//getting homepage
+	String homepageUrl = firstResult.getShowHomepageUrl();
+	TvcomShowHomepage homepage = new TvcomShowHomepage(homepageUrl);
+
+	//getting first season guide
+	TvcomSeasonGuide currentSeasonGuide = new TvcomSeasonGuide(TvcomUtils.getSeasonGuideLinkFromHomepageLink(homepageUrl, 1));
+
+	//getting seasons number
+	int seasonsNumber = currentSeasonGuide.getSeasonsNumber();
+
+	//creating show
+	ShowBuilder showBuilder = new ShowBuilder();
+	ShowTvcomInfo showInfoProvider = new ShowTvcomInfo(firstResult, homepage, currentSeasonGuide);
+	Show show = showBuilder.getShow(showInfoProvider);
+
+	//listing through all seasons and episodes
+	for (int currentSeasonOrdinal = 1; currentSeasonOrdinal <= seasonsNumber; currentSeasonOrdinal++) {
+
+	    currentSeasonGuide = new TvcomSeasonGuide(TvcomUtils.getSeasonGuideLinkFromHomepageLink(homepageUrl, currentSeasonOrdinal));
+
+	    //creating season
+	    SeasonBuilder seasonBuilder = new SeasonBuilder();
+	    SeasonTvcomInfo seasonInfoProvider = new SeasonTvcomInfo(currentSeasonGuide);
+	    Season season = seasonBuilder.getSeason(seasonInfoProvider);
+
+	    //assigning season
+	    show.edit().addSeason(season);
+
+	    for (int ordinal = 1; ordinal <= currentSeasonGuide.getEpisodesNumber(); ordinal++) {
+		//creating episode
+		EpisodeBuilder episodeBuilder = new EpisodeBuilder();
+		EpisodeTvcomInfo episodeIinfoProvider = new EpisodeTvcomInfo(currentSeasonGuide, ordinal);
+		Episode episode = episodeBuilder.getEpisode(episodeIinfoProvider);
+
+		//assigning episode
+		season.edit().addEpisode(episode);
+	    }
+
+	}
+
+	this.show = show;
+
+	//System.out.println(season_1);
+    }
+
+    public Show getShow() {
+	if (show == null) {
+	    throw new DebugError("Create show first");
+	}
+	return show;
+    }
+}
