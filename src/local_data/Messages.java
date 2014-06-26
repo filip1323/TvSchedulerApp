f7a46d8d3206c9d1871a85e9019281ca311c3209
toddler2584 <toddler2584@gmail.com>
2014-06-26 23:44:28 +0200

    sketch

--- src/local_data/Messages.java
+++ src/local_data/Messages.java
@@ -11,4 +11,24 @@ package local_data;
  */
 public class Messages {
 
+    public static final String OPTION_AUTOSTART = "Włączaj przy starcie systemu windows.";
+
+    public static final String OPTION_AUTOREFRESH = "Automatycznie aktualizuj informacje o serialach.";
+
+    public static final String OPTION_DEBUG = "Pomóż rozwijać aplikację, przez wysyłanie danych o błędach.";
+
+    public static final String OPTION_CONNECT_PIRATEBAY = "Połącz z thepiratebay.";
+
+    public static final String OPTION_CONNECT_EKINO = "Połącz z ekinem.tv";
+
+    public static final String NOTIFICATION_NEXT_EP_COUNTER = "Pokazuj ile dni zostało do następnego epizodu.";
+
+    public static final String NOTIFICATION_NEXT_EP_ANNOUNCEMENT = "Powiadamiaj o zapowiedzi nowego odcinka.";
+
+    public static final String NOTIFICATION_NEXT_EP_RELEASED_TODAY = "Powiadamiaj o premierze nowego odcinka.";
+
+    public static final String NOTIFICATION_NEXT_SEAS_RELEASED_TODAY = "Powiadamiaj o premierze nowego sezonu.";
+
+    public static final String NOTIFICATION_UPDATE = "Powiadamiaj o dostępności nowej wersji programu.";
+;
 }
