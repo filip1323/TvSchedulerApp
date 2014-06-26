d08d02736966e46ce721ebfec8ede455a0bf694d
toddler2584 <toddler2584@gmail.com>
2014-06-26 23:45:59 +0200

    development

--- src/show_components/ShowController.java
+++ src/show_components/ShowController.java
@@ -5,10 +5,70 @@
  */
 package show_components;
 
+import java.util.ArrayList;
+import show_components.show.Show;
+import user_exceptions.DebugError;
+import user_interface.UserInterface;
+
 /**
  *
  * @author Filip
  */
-public class ShowControllerFacade {
+public class ShowController implements ControllerInterface {
 
+    private final ArrayList<Show> storedShows;
+    private final ShowLocalDataHUB showLocalDataHUB;
+    private final ArrayList<String> storedShowsTitle;
+    private UserInterface userInterface;
+
+    public ShowController() {
+	storedShows = new ArrayList<>();
+	showLocalDataHUB = new ShowLocalDataHUB();
+	storedShowsTitle = showLocalDataHUB.getLoadedTitles();
+	for (String title : storedShowsTitle) {
+	    storedShows.add(showLocalDataHUB.load(title));
+	}
+    }
+
+    public void assignUserInterface(UserInterface ui) {
+	this.userInterface = ui;
+    }
+
+    @Override
+    public Show getShowWithTitle(String title) {
+	if (!storedShowsTitle.contains(title)) {
+	    throw new DebugError(title + " is not stored");
+	}
+	for (Show show : storedShows) {
+	    if (show.getTitle().equals(title)) {
+		return show;
+	    }
+	}
+	return null;
+    }
+
+    @Override
+    public ArrayList<Show> getStoredShows() {
+	return storedShows;
+    }
+
+    @Override
+    public void removeShow(Show show) {
+	showLocalDataHUB.remove(show.getTitle());
+    }
+
+    @Override
+    public void saveShow(Show show) {
+	showLocalDataHUB.save(show);
+    }
+
+    @Override
+    public boolean showStored(Show show) {
+	return (storedShowsTitle.contains(show.getTitle()));
+    }
+
+    @Override
+    public void updateShow(Show show) {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
